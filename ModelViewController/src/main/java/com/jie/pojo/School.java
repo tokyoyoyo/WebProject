7eb8d7ee11ec8e59e06b2508780c91d6223f7da4
tokyoyoyo <1278441739@qq.com>
2021-06-07 18:09:39 +0800

    学习了JSP,下一步学习MVC框架知识

---
+++ ModelViewController/src/main/java/com/jie/pojo/School.java
@@ -0,0 +1,35 @@
+package com.jie.pojo;
+
+public class School {
+    public String name;
+    public String address;
+
+    @Override
+    public String toString() {
+        return "School{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public School(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+}
